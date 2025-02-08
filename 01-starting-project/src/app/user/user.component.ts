d99087d97f9483b9f91bd1f5c1e5614584da92c0
yana.radeva7 <yana.radeva7@gmail.com>
2025-02-08 19:34:25 +0200

    introduce outputs and data emitting; remove input signals

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Input, input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,17 +7,16 @@ import { Component, computed, Input, input } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  // @Input({ required: true }) avatar!: string;
-  // @Input({ required: true }) name!: string;
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
 
-  avatar = input.required<string>();
-  name = input.required<string>();
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
-  imagePath = computed(() => 'assets/users/' + this.avatar());
-
-  // get imagePath() {
-  //   return 'assets/users/' + this.avatar;
-  // }
-
-  onSelectUser() { }
+  onSelectUser() { 
+    this.select.emit(this.id);
+  }
 }
