57d30fa00802afa2363cd46c7b9e58d646c312bf
yana.radeva7 <yana.radeva7@gmail.com>
2025-02-08 20:32:54 +0200

    use the output() function as an alternative

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -10,7 +10,8 @@ export class UserComponent {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-  @Output() select = new EventEmitter();
+  //@Output() select = new EventEmitter();
+  select = output<string>();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
