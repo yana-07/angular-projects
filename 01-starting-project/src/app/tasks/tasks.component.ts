5531f3b34a31428d51aa6abeb0f82055a098c003
yana.radeva7 <yana.radeva7@gmail.com>
2025-02-09 13:41:27 +0200

    add task component

--- 01-starting-project/src/app/tasks/tasks.component.ts
+++ 01-starting-project/src/app/tasks/tasks.component.ts
@@ -1,11 +1,22 @@
 import { Component, Input } from '@angular/core';
 
+import { TaskComponent } from './task/task.component';
+
+import { DUMMY_TASKS } from './dummy-tasks';
+
 @Component({
   selector: 'app-tasks',
   standalone: true,
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input() name: string | undefined;
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) name!: string;
+  tasks = DUMMY_TASKS;
+
+  get selectedUserTasts() {
+    return this.tasks.filter(task => task.userId === this.userId);
+  }
 }
