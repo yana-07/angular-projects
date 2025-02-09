db4946931eb4776ae4bc005b99d13705513fb0d2
yana.radeva7 <yana.radeva7@gmail.com>
2025-02-09 14:03:50 +0200

    use class binding for dynamic styling

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -10,6 +10,7 @@ import { type User } from './user.model';
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
