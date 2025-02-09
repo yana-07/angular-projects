9a5f2a6af5b85604a7d26a60a9ec708da1ff5761
yana.radeva7 <yana.radeva7@gmail.com>
2025-02-09 18:01:51 +0200

    introduce services and dependency injection

--- 01-starting-project/src/app/tasks/tasks.component.ts
+++ 01-starting-project/src/app/tasks/tasks.component.ts
@@ -2,48 +2,32 @@ import { Component, Input, Output } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { NewTaskComponent } from './new-task/new-task.component';
+import { TasksService } from './tasks.service';
 import { type NewTaskData } from './task/task.model';
-
-import { DUMMY_TASKS } from './dummy-tasks';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+  styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
   isAddingTask = false;
-  tasks = DUMMY_TASKS;
+
+  constructor(private tasksService: TasksService) {}
 
   get selectedUserTasts() {
-    return this.tasks.filter(task => task.userId === this.userId);
-  }
-
-  onCompleteTask(id: string) {
-    this.tasks = this.tasks.filter(task => task.id !== id);
+    return this.tasksService.getUserTasks(this.userId);
   }
 
   onStartAddTask() {
     this.isAddingTask = true;
   }
 
-  onCancelAddTask() {
-    this.isAddingTask = false;
-  }
-
-  onAddTask(taskData: NewTaskData) {
-    this.tasks.unshift({
-      id: new Date().getTime().toString(),
-      userId: this.userId,
-      title: taskData.title,
-      summary: taskData.summary,
-      dueDate: taskData.date
-    });
-
+  onCloseAddTask() {
     this.isAddingTask = false;
   }
 }
