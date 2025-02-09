3b2d9c4ffe45b2355daa63233dda343d59d81731
yana.radeva7 <yana.radeva7@gmail.com>
2025-02-09 16:47:15 +0200

    introduce code projection using ng-content

--- 01-starting-project/src/app/tasks/task/task.component.ts
+++ 01-starting-project/src/app/tasks/task/task.component.ts
@@ -1,10 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { type Task } from './task.model'
+import { CardComponent } from '../../shared/card/card.component';
 
 @Component({
   selector: 'app-task',
   standalone: true,
+  imports: [CardComponent],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
