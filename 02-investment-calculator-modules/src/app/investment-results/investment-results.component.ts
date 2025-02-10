5a9c0cbdc0bc4e0b602655618ac3f07b8c4722d2
yana.radeva7 <yana.radeva7@gmail.com>
2025-02-10 20:30:28 +0200

    duplicate project to migrate to modules

---
+++ 02-investment-calculator-modules/src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,19 @@
+import { CurrencyPipe } from '@angular/common';
+import { Component, inject, computed } from '@angular/core';
+
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [CurrencyPipe],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css'
+})
+export class InvestmentResultsComponent {
+  private investmentService = inject(InvestmentService);
+
+  // 'results' is now a computed readonly signal - makes sure that the data managed by the service cannot be modified from outside the service
+  // results = this.investmentService.resultData.asReadonly(); -  alternative
+  results = computed(() => this.investmentService.resultData());
+}
