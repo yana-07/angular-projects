bb4741fcb7be0a90dd471a9cf05974e71a612cdc
Yana Radeva <yana.radeva7@gmail.com>
2025-02-20 11:41:00 +0200

    introduce template variables

--- 03-components-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 03-components-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
@@ -6,10 +7,12 @@ import { ControlComponent } from '../../../shared/control/control.component';
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-
+  onSubmit(title: string, ticketText: string, form: HTMLFormElement) {
+    form.reset();
+  }
 }
