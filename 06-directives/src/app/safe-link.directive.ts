9e89fc2b149b11b73626c14ca53044c572ebde3d
Yana Radeva <yana.radeva7@gmail.com>
2025-02-20 17:23:54 +0200

    add custom safeLink directive

---
+++ 06-directives/src/app/safe-link.directive.ts
@@ -0,0 +1,24 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: 'a[appSafeLink]',
+  standalone: true,
+  host: {
+    '(click)': 'onConfirmLeavePage($event)',
+  },
+})
+export class SafeLinkDirective {
+  constructor() {
+    console.log('SafeLinkDirective is active.');
+  }
+
+  onConfirmLeavePage(event: MouseEvent) {
+    const wantsToLeave = window.confirm(
+      'Are you sure you want to leave the app?'
+    );
+
+    if (wantsToLeave) return;
+
+    event.preventDefault();
+  }
+}
