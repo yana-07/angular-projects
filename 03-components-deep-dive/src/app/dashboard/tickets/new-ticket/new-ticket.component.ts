0c1f8100675fc6b31c543aece7323a4fd1509177
Yana Radeva <yana.radeva7@gmail.com>
2025-02-20 11:53:49 +0200

    get access to template elements using the ViewChild decorator

--- 03-components-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 03-components-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { ButtonComponent } from '../../../shared/button/button.component';
@@ -12,7 +12,12 @@ import { ControlComponent } from '../../../shared/control/control.component';
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-  onSubmit(title: string, ticketText: string, form: HTMLFormElement) {
-    form.reset();
+  // @ViewChild(ButtonComponent) form: ButtonComponent | undefined; -> directive classes passed as arguments possible
+  // When the component class is instantiated, no component View will exist yet, therefore the initial value will
+  // be undefined and will be populated once Angular has initialized the component View
+  @ViewChild('form') form: ElementRef<HTMLFormElement> | undefined; // not a css selector but the name of the template variable without the '#'
+
+  onSubmit(title: string, ticketText: string) {
+    this.form?.nativeElement.reset();
   }
 }
