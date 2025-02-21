18c1141cd24989d8e062d5fdc042debd2432914c
Yana Radeva <yana.radeva7@gmail.com>
2025-02-21 15:24:03 +0200

    demonstrate an alternative (non-optimal) way of registering services with the Application Root Environment Injector; introduce the Element Injector

--- 08-services-dep-injection/src/main.ts
+++ 08-services-dep-injection/src/main.ts
@@ -3,3 +3,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
+
+// bootstrapApplication(AppComponent, {
+//     providers: [TasksService]
+// }).catch((err) => console.error(err));
