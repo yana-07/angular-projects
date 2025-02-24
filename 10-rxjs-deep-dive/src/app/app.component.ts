31b10934d0992befbf8d7b4e968e1669b596e8ec
Yana Radeva <yana.radeva7@gmail.com>
2025-02-24 16:20:26 +0200

    set up an observable and introduce the "map" operator function

--- 10-rxjs-deep-dive/src/app/app.component.ts
+++ 10-rxjs-deep-dive/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+
+import { interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
 
+  ngOnInit() {
+    // at least one subscriber is needed to kick off the interval
+    const subscription = interval(1000)
+      .pipe(
+        map((val: number) => val * 2)
+      )
+      .subscribe({
+        next: (val) => console.log(val),
+      });
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
 }
