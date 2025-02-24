44619c4dd8de505b62e28c92b8be39e00bca51bb
Yana Radeva <yana.radeva7@gmail.com>
2025-02-24 14:49:27 +0200

    set up a subscription and output behavior subject values in the template using the async pipe

--- 09-change-detection/src/app/messages/messages-list/messages-list.component.ts
+++ 09-change-detection/src/app/messages/messages-list/messages-list.component.ts
@@ -1,33 +1,36 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { MessagesService } from '../messages.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-messages-list',
   standalone: true,
   templateUrl: './messages-list.component.html',
   styleUrl: './messages-list.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [AsyncPipe]
 })
-export class MessagesListComponent implements OnInit {
+export class MessagesListComponent {// implements OnInit {
   private messagesService = inject(MessagesService);
-  private cdRef = inject(ChangeDetectorRef);
-  private destoyRef = inject(DestroyRef);
+  messages$ = this.messagesService.messages$;
+  // private cdRef = inject(ChangeDetectorRef);
+  // private destoyRef = inject(DestroyRef);
   //messages = this.messagesService.allMessages;
   // get messages() {
   //   return this.messagesService.allMessages;
   // }
-  messages: string[] = [];
+  //messages: string[] = [];
 
-  ngOnInit() {
-    const subsription = this.messagesService.messages$.subscribe((messages) => {
-      this.messages = messages;
-      // make sure that change detection runs for this component
-      this.cdRef.markForCheck();
-    });
-    this.destoyRef.onDestroy(() => {
-      subsription.unsubscribe();
-    });
-  }
+  // ngOnInit() {
+  //   const subsription = this.messagesService.messages$.subscribe((messages) => {
+  //     this.messages = messages;
+  //     // make sure that change detection runs for this component
+  //     this.cdRef.markForCheck();
+  //   });
+  //   this.destoyRef.onDestroy(() => {
+  //     subsription.unsubscribe();
+  //   });
+  // }
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
