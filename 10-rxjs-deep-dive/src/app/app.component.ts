3a9923b7dad650ccbe7ce3da2b095e909aa23b8b
Yana Radeva <yana.radeva7@gmail.com>
2025-02-24 16:50:55 +0200

    use signals instead of observables

--- 10-rxjs-deep-dive/src/app/app.component.ts
+++ 10-rxjs-deep-dive/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { Component, DestroyRef, effect, inject, OnInit, signal, computed } from '@angular/core';
 
 import { interval, map } from 'rxjs';
 
@@ -8,18 +8,35 @@ import { interval, map } from 'rxjs';
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
-  private destroyRef = inject(DestroyRef);
+  //private destroyRef = inject(DestroyRef);
+  clickCount = signal(0);
+  interval = signal(0);
+  doubleInterval = computed(() => this.interval() * 2);
+
+  constructor() {
+    effect(() => {
+      console.log(`Clicked button ${this.clickCount()} times.`)
+      console.log(this.doubleInterval());
+    });
+  }
 
   ngOnInit() {
+    setInterval(() => {
+      this.interval.update(prevIntervalNum => prevIntervalNum + 1);
+    }, 1000);
     // at least one subscriber is needed to kick off the interval
-    const subscription = interval(1000)
-      .pipe(
-        map((val: number) => val * 2)
-      )
-      .subscribe({
-        next: (val) => console.log(val),
-      });
+    // const subscription = interval(1000)
+    //   .pipe(
+    //     map((val: number) => val * 2)
+    //   )
+    //   .subscribe({
+    //     next: (val) => console.log(val),
+    //   });
 
-    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+    // this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
+
+  onClick() {
+    this.clickCount.update(prevCount => prevCount + 1);
   }
 }
