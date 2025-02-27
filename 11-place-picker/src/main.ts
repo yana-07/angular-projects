a1dc4c4fc230071b905a3b1ac351e71081719985
Yana Radeva <yana.radeva7@gmail.com>
2025-02-27 17:42:31 +0200

    introduce http request and response interceptors

--- 11-place-picker/src/main.ts
+++ 11-place-picker/src/main.ts
@@ -1,9 +1,34 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  HttpEventType,
+  HttpHandlerFn,
+  HttpRequest,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
 
 import { AppComponent } from './app/app.component';
+import { tap } from 'rxjs';
 
+function loggingInterceptor(
+  request: HttpRequest<unknown>,
+  next: HttpHandlerFn
+) {
+  // const req = request.clone({
+  //     headers: request.headers.set('X-DEBUG', 'TESTING')
+  // });
+  console.log('[Outgoing Request]', request);
+  return next(request).pipe(
+    tap({
+      next: (event) => {
+        if (event.type === HttpEventType.Response) {
+          console.log('[Incoming Response]', event.status, event.body);
+        }
+      },
+    })
+  );
+}
 
 bootstrapApplication(AppComponent, {
-    providers: [provideHttpClient()]
+  providers: [provideHttpClient(withInterceptors([loggingInterceptor]))],
 }).catch((err) => console.error(err));
