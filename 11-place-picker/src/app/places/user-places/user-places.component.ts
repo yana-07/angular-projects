df939b231d47c1c7e75464602c26640058b5ac68
Yana Radeva <yana.radeva7@gmail.com>
2025-02-27 09:05:45 +0200

    remove user places via http delete request

--- 11-place-picker/src/app/places/user-places/user-places.component.ts
+++ 11-place-picker/src/app/places/user-places/user-places.component.ts
@@ -34,4 +34,12 @@ export class UserPlacesComponent implements OnInit {
 
     this.destroyRef.onDestroy(() => subscription.unsubscribe());
   }
+
+  onRemovePlace(place: Place) {
+    const subscription = this.placesService
+      .removeUserPlace(place)
+      .subscribe();
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
 }
