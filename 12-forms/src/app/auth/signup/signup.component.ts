fa63a4d7dbd62a819eb7acc02a3d6ae4e1f1bd91
Yana Radeva <yana.radeva7@gmail.com>
2025-03-05 10:08:12 +0200

    work with nested form groups

--- 12-forms/src/app/auth/signup/signup.component.ts
+++ 12-forms/src/app/auth/signup/signup.component.ts
@@ -13,18 +13,22 @@ export class SignupComponent {
     email: new FormControl('', {
       validators: [Validators.required, Validators.email]
     }),
-    password: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(6)]
-    }),
-    confirmPassword: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(6)]
+    passwords: new FormGroup({
+      password: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(6)]
+      }),
+      confirmPassword: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(6)]
+      })
     }),
     firstName: new FormControl('', { validators: [Validators.required ]}),
     lastName: new FormControl('', { validators: [Validators.required ]}),
-    street: new FormControl('', { validators: [Validators.required ]}),
-    number: new FormControl('', { validators: [Validators.required ]}),
-    postalCode: new FormControl('', { validators: [Validators.required ]}),
-    city: new FormControl('', { validators: [Validators.required] }),
+    address: new FormGroup({
+      street: new FormControl('', { validators: [Validators.required ]}),
+      number: new FormControl('', { validators: [Validators.required ]}),
+      postalCode: new FormControl('', { validators: [Validators.required ]}),
+      city: new FormControl('', { validators: [Validators.required] })
+    }),
     role: new FormControl<'student' | 'teacher' | 'employee' | 'founder' | 'other'>('student', { validators: [Validators.required] }),
     agree: new FormControl(false, { validators: [Validators.required] })
   });
