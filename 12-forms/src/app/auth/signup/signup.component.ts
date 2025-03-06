51ba4b6a9de5fb13bfeaab7e741015692d5290a3
Yana Radeva <yana.radeva7@gmail.com>
2025-03-06 09:49:02 +0200

    create form group validators

--- 12-forms/src/app/auth/signup/signup.component.ts
+++ 12-forms/src/app/auth/signup/signup.component.ts
@@ -1,5 +1,25 @@
 import { Component } from '@angular/core';
-import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormArray,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+function equalValues(controlName1: string, controlName2: string) {
+  return (control: AbstractControl) => {
+    const val1 = control.get(controlName1)?.value;
+    const val2 = control.get(controlName2)?.value;
+
+    if (val1 === val2) {
+      return null;
+    }
+
+    return { valuesNotEqual: true };
+  };
+}
 
 @Component({
   selector: 'app-signup',
@@ -13,21 +33,26 @@ export class SignupComponent {
     email: new FormControl('', {
       validators: [Validators.required, Validators.email]
     }),
-    passwords: new FormGroup({
-      password: new FormControl('', {
-        validators: [Validators.required, Validators.minLength(6)]
-      }),
-      confirmPassword: new FormControl('', {
-        validators: [Validators.required, Validators.minLength(6)]
-      })
-    }),
-    firstName: new FormControl('', { validators: [Validators.required ]}),
-    lastName: new FormControl('', { validators: [Validators.required ]}),
+    passwords: new FormGroup(
+      {
+        password: new FormControl('', {
+          validators: [Validators.required, Validators.minLength(6)]
+        }),
+        confirmPassword: new FormControl('', {
+          validators: [Validators.required, Validators.minLength(6)]
+        }),
+      },
+      {
+        validators: [equalValues('password', 'confirmPassword')]
+      }
+    ),
+    firstName: new FormControl('', { validators: [Validators.required] }),
+    lastName: new FormControl('', { validators: [Validators.required] }),
     address: new FormGroup({
-      street: new FormControl('', { validators: [Validators.required ]}),
-      number: new FormControl('', { validators: [Validators.required ]}),
-      postalCode: new FormControl('', { validators: [Validators.required ]}),
-      city: new FormControl('', { validators: [Validators.required] })
+      street: new FormControl('', { validators: [Validators.required] }),
+      number: new FormControl('', { validators: [Validators.required] }),
+      postalCode: new FormControl('', { validators: [Validators.required] }),
+      city: new FormControl('', { validators: [Validators.required] }),
     }),
     role: new FormControl<'student' | 'teacher' | 'employee' | 'founder' | 'other'>('student', { validators: [Validators.required] }),
     source: new FormArray([
