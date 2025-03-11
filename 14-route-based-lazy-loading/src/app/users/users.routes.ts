414c56a7a1ba0c51acad4e9d69751a9ec8263638
Yana Radeva <yana.radeva7@gmail.com>
2025-03-11 17:37:37 +0200

    lazy load entire route groups

--- 14-route-based-lazy-loading/src/app/users/users.routes.ts
+++ 14-route-based-lazy-loading/src/app/users/users.routes.ts
@@ -1,30 +1,7 @@
-import { inject } from '@angular/core';
-import { ResolveFn, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 
 import { NewTaskComponent, canLeaveEditPage } from '../tasks/new-task/new-task.component';
-import { Task } from '../tasks/task/task.model';
-import { TasksService } from '../tasks/tasks.service';
-
-const resolveUserTasks: ResolveFn<Task[]> = (
-  activatedRouteSnapshot,
-  routerState
-) => {
-  const order = activatedRouteSnapshot.queryParams['order'];
-  const tasksService = inject(TasksService);
-  const tasks = tasksService
-    .allTasks()
-    .filter(
-      (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
-    );
-
-  if (order && order === 'asc') {
-    tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
-  } else {
-    tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
-  }
-
-  return tasks.length ? tasks : [];
-};
+import { resolveUserTasks, TasksComponent } from '../tasks/tasks.component';
 
 export const routes: Routes = [
   {
@@ -34,8 +11,9 @@ export const routes: Routes = [
   },
   {
     path: 'tasks', // <your-domain>/users/<uid>/tasks
-    loadComponent: () => 
-      import('../tasks/tasks.component').then(module => module.TasksComponent),
+    component: TasksComponent,
+    // loadComponent: () => 
+    //   import('../tasks/tasks.component').then(module => module.TasksComponent),
     runGuardsAndResolvers: 'always',
     resolve: {
       userTasks: resolveUserTasks,
